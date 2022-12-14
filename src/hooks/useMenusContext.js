cfb312b91367e67b8b57ef8d7d943c961a944ebd
Valentina Popova <solval89@gmail.com>
2022-12-14 23:10:27 +0200

    Fix typos

--- src/hooks/useMenusContext.js
+++ src/hooks/useMenusContext.js
@@ -4,7 +4,7 @@ import { MenusContext } from "../context/MenuContext"
 export const useMenusContext = () => {
     const context = useContext(MenusContext)
     if (!context) {
-        throw Error('useMenusContext must be used insude an MenusContextProvider')
+        throw Error('useMenusContext must be used inside an MenusContextProvider')
     }
     return context
 }
--- src/hooks/useRecipesContext.js
+++ src/hooks/useRecipesContext.js
@@ -4,7 +4,7 @@ import { RecipeContext } from "../context/RecipeContext"
 export const useRecipesContext = () => {
     const context = useContext(RecipeContext)
     if (!context) {
-        throw Error('useRecipesContext must be used insude an RecipesContextProvider')
+        throw Error('useRecipesContext must be used inside an RecipesContextProvider')
     }
     return context
 }
